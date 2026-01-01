862a09efa9b5733cac1bb82fb65fdccb89b8cf7b
Steve Holden <steve@holdenweb.com>
2026-01-01 22:05:50 +0000

    Move url_capture to ingester.

--- url_capture.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def get_chrome_url_mac():
-    script = 'tell application "Google Chrome" to return URL of active tab of front window'
-    url = os.popen(f"osascript -e '{script}'").read().strip()
-    return url
-
-print(f"Current URL: {get_chrome_url_mac()}")
